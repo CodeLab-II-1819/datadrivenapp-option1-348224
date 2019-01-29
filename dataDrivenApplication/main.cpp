801c152e468f9169c7f0cc98da1ed37b93479ebd
348224 <charles.roffe17@bathspa.ac.uk>
2019-01-29 18:19:42 +0000

    0.2.0
    Program now working as intended. May return later for small additions to code.
    - Added a 'while loop' to the program that allows the user to restart the program without quitting.

--- dataDrivenApplication/main.cpp
+++ dataDrivenApplication/main.cpp
@@ -6,180 +6,189 @@ using namespace std;
 
 
 int input;
+char replay = 'y';
 
 
 int main() {
 
-	cout << "Please select an option:" << endl;
+	while (replay == 'y') {
 
-	cout << "1/ Display number of tweets from sample." << endl;
-	cout << "2/ Display number of tweets that mention money" << endl;
-	cout << "3/ Display number of tweets that mention politics." << endl;
-	cout << "4/ Display all tweets that mention 'Paris'." << endl;
-	cout << "5/ Display all tweets that mention 'DreamWorks'." << endl;
-	cout << "6/ Display all tweets that mention 'Uber'." << endl;
-	cout << "7/ Display all tweets that mention 'Trump'." << endl;
-	cout << "8/ Display all tweets that mention 'Obama'." << endl;
-	cout << "9/ Display all tweets that mention 'Dogs'." << endl;
-	cout << "10/ Display all tweets that mention 'Christmas'." << endl;
+		cout << "Please select an option:" << endl;
 
-	cin >> input;
+		cout << "1/ Display number of tweets from sample." << endl;
+		cout << "2/ Display number of tweets that mention money" << endl;
+		cout << "3/ Display number of tweets that mention politics." << endl;
+		cout << "4/ Display all tweets that mention 'Paris'." << endl;
+		cout << "5/ Display all tweets that mention 'DreamWorks'." << endl;
+		cout << "6/ Display all tweets that mention 'Uber'." << endl;
+		cout << "7/ Display all tweets that mention 'Trump'." << endl;
+		cout << "8/ Display all tweets that mention 'Obama'." << endl;
+		cout << "9/ Display all tweets that mention 'Dogs'." << endl;
+		cout << "10/ Display all tweets that mention 'Christmas'." << endl;
 
-	int count = 0;
-	string data;
+		cin >> input;
+
+		int count = 0;
+		string data;
 
 
-	if (input == 1) {
-		
-		ifstream inFile;
-		inFile.open("sampleTweets.csv");
-		cout << "Reading file..." << endl;
-		if (inFile.good()) {
-			while (!inFile.eof()) {
-				getline(inFile, data);
-				count++;
-			}
-			inFile.close();
-			cout << count << endl;
-		}	
-	}
-	else if (input == 2) {
-		
-		ifstream inFile;
-		inFile.open("sampleTweets.csv");
-		cout << "Reading file..." << endl;
-		if (inFile.good()) {
-			while (!inFile.eof()) {
-				getline(inFile, data);
-				if (data.find("money")<=data.length()) {
+		if (input == 1) {
+
+			ifstream inFile;
+			inFile.open("sampleTweets.csv");
+			cout << "Reading file..." << endl;
+			if (inFile.good()) {
+				while (!inFile.eof()) {
+					getline(inFile, data);
 					count++;
 				}
+				inFile.close();
+				cout << count << endl;
 			}
-			inFile.close();
-			cout << count << endl;
-		}	
-	}
-	else if (input == 3) {
-
-		ifstream inFile;
-		inFile.open("sampleTweets.csv");
-		cout << "Reading file..." << endl;
-		if (inFile.good()) {
-			while (!inFile.eof()) {
-				getline(inFile, data);
-				if (data.find("politics") <= data.length()) {
-					count++;
-				}
-			}
-			inFile.close();
-			cout << count << endl;
 		}
-	}
-	else if (input == 4) {
+		else if (input == 2) {
 
-		ifstream inFile;
-		inFile.open("sampleTweets.csv");
-		cout << "Reading file..." << endl;
-		if (inFile.good()) {
-			while (!inFile.eof()) {
-				getline(inFile, data);
-				if (data.find("Paris") <= data.length()) {
-					cout << data << endl;
+			ifstream inFile;
+			inFile.open("sampleTweets.csv");
+			cout << "Reading file..." << endl;
+			if (inFile.good()) {
+				while (!inFile.eof()) {
+					getline(inFile, data);
+					if (data.find("money") <= data.length()) {
+						count++;
+					}
 				}
+				inFile.close();
+				cout << count << endl;
 			}
-			inFile.close();
 		}
-	}
-	else if (input == 5) {
+		else if (input == 3) {
 
-		ifstream inFile;
-		inFile.open("sampleTweets.csv");
-		cout << "Reading file..." << endl;
-		if (inFile.good()) {
-			while (!inFile.eof()) {
-				getline(inFile, data);
-				if (data.find("DreamWorks") <= data.length()) {
-					cout << data << endl;
+			ifstream inFile;
+			inFile.open("sampleTweets.csv");
+			cout << "Reading file..." << endl;
+			if (inFile.good()) {
+				while (!inFile.eof()) {
+					getline(inFile, data);
+					if (data.find("politics") <= data.length()) {
+						count++;
+					}
 				}
+				inFile.close();
+				cout << count << endl;
 			}
-			inFile.close();
 		}
-	}
-	else if (input == 6) {
+		else if (input == 4) {
 
-		ifstream inFile;
-		inFile.open("sampleTweets.csv");
-		cout << "Reading file..." << endl;
-		if (inFile.good()) {
-			while (!inFile.eof()) {
-				getline(inFile, data);
-				if (data.find("Uber") <= data.length()) {
-					cout << data << endl;
+			ifstream inFile;
+			inFile.open("sampleTweets.csv");
+			cout << "Reading file..." << endl;
+			if (inFile.good()) {
+				while (!inFile.eof()) {
+					getline(inFile, data);
+					if (data.find("Paris") <= data.length()) {
+						cout << data << endl;
+					}
 				}
+				inFile.close();
 			}
-			inFile.close();
 		}
-	}
-	else if (input == 7) {
+		else if (input == 5) {
 
-		ifstream inFile;
-		inFile.open("sampleTweets.csv");
-		cout << "Reading file..." << endl;
-		if (inFile.good()) {
-			while (!inFile.eof()) {
-				getline(inFile, data);
-				if (data.find("Trump") <= data.length()) {
-					cout << data << endl;
+			ifstream inFile;
+			inFile.open("sampleTweets.csv");
+			cout << "Reading file..." << endl;
+			if (inFile.good()) {
+				while (!inFile.eof()) {
+					getline(inFile, data);
+					if (data.find("DreamWorks") <= data.length()) {
+						cout << data << endl;
+					}
 				}
+				inFile.close();
 			}
-			inFile.close();
 		}
-	}
-	else if (input == 8) {
+		else if (input == 6) {
 
-		ifstream inFile;
-		inFile.open("sampleTweets.csv");
-		cout << "Reading file..." << endl;
-		if (inFile.good()) {
-			while (!inFile.eof()) {
-				getline(inFile, data);
-				if (data.find("Obama") <= data.length()) {
-					cout << data << endl;
+			ifstream inFile;
+			inFile.open("sampleTweets.csv");
+			cout << "Reading file..." << endl;
+			if (inFile.good()) {
+				while (!inFile.eof()) {
+					getline(inFile, data);
+					if (data.find("Uber") <= data.length()) {
+						cout << data << endl;
+					}
 				}
+				inFile.close();
 			}
-			inFile.close();
 		}
-	}
-	else if (input == 9) {
+		else if (input == 7) {
 
-		ifstream inFile;
-		inFile.open("sampleTweets.csv");
-		cout << "Reading file..." << endl;
-		if (inFile.good()) {
-			while (!inFile.eof()) {
-				getline(inFile, data);
-				if (data.find("dog") <= data.length()) {
-					cout << data << endl;
+			ifstream inFile;
+			inFile.open("sampleTweets.csv");
+			cout << "Reading file..." << endl;
+			if (inFile.good()) {
+				while (!inFile.eof()) {
+					getline(inFile, data);
+					if (data.find("Trump") <= data.length()) {
+						cout << data << endl;
+					}
 				}
+				inFile.close();
 			}
-			inFile.close();
 		}
-	}
-	else if (input == 10) {
+		else if (input == 8) {
 
-		ifstream inFile;
-		inFile.open("sampleTweets.csv");
-		cout << "Reading file..." << endl;
-		if (inFile.good()) {
-			while (!inFile.eof()) {
-				getline(inFile, data);
-				if (data.find("Christmas") <= data.length()) {
-					cout << data << endl;
+			ifstream inFile;
+			inFile.open("sampleTweets.csv");
+			cout << "Reading file..." << endl;
+			if (inFile.good()) {
+				while (!inFile.eof()) {
+					getline(inFile, data);
+					if (data.find("Obama") <= data.length()) {
+						cout << data << endl;
+					}
 				}
+				inFile.close();
 			}
-			inFile.close();
 		}
+		else if (input == 9) {
+
+			ifstream inFile;
+			inFile.open("sampleTweets.csv");
+			cout << "Reading file..." << endl;
+			if (inFile.good()) {
+				while (!inFile.eof()) {
+					getline(inFile, data);
+					if (data.find("dog") <= data.length()) {
+						cout << data << endl;
+					}
+				}
+				inFile.close();
+			}
+		}
+		else if (input == 10) {
+
+			ifstream inFile;
+			inFile.open("sampleTweets.csv");
+			cout << "Reading file..." << endl;
+			if (inFile.good()) {
+				while (!inFile.eof()) {
+					getline(inFile, data);
+					if (data.find("Christmas") <= data.length()) {
+						cout << data << endl;
+					}
+				}
+				inFile.close();
+			}
+		}
+
+		cout << "Do you want to continue? (Y/N)" << endl;
+		cin >> replay;
+
 	}
+
 	system("pause");
 }
 
