9cc941d66de0cc9b323e66ab8abe5702dcc27343
348224 <charles.roffe17@bathspa.ac.uk>
2019-01-31 16:18:04 +0000

    0.2.3
    Not much changed between now and previous version. Tried to change font colour to black and move text to appropriate areas, but hasn't been working whevever I try.
    
    May have to be final version.

--- gui/src/ofApp.cpp
+++ gui/src/ofApp.cpp
@@ -10,22 +10,22 @@ void ofApp::setup(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	baseGUI.draw(1024, 768, 0, 0);
-	titleFont.drawString("Tweet Finder", 600, 60);
-	mainFont.drawString("Choose a topic on the left to view all recent tweets regarding that topic:", 600, 120);
-	mainFont.drawString("Current Topic:", 100, 20);
-	mainFont.drawString("Paris", 100, 70);
-	mainFont.drawString("All", 100, 140);
-	mainFont.drawString("Money", 100, 200);
-	mainFont.drawString("Politics", 100, 260);
-	mainFont.drawString("Paris", 100, 320);
-	mainFont.drawString("DreamWorks", 100, 380);
-	mainFont.drawString("Uber", 100, 440);
-	mainFont.drawString("Trump", 100, 500);
-	mainFont.drawString("Obama", 100, 560);
-	mainFont.drawString("Dogs", 100, 620);
-	mainFont.drawString("Christmas", 100, 680);
-	mainFont.drawString("Total Count: - number of tweets -", 400, 200);
-	tweetFont.drawString("- list of all tweets -", 400, 200);
-
+	baseGUI.draw(0, 0, 1024, 768);
+	ofSetColor(0, 0, 0);
+	titleFont.drawString("Tweet Finder", 400, 60);
+	mainFont.drawString("Choose a topic on the left to view all recent tweets regarding that topic:", 400, 120);
+	mainFont.drawString("Current Topic:", 50, 20);
+	mainFont.drawString("Paris", 50, 70);
+	mainFont.drawString("All", 50, 140);
+	mainFont.drawString("Money", 50, 200);
+	mainFont.drawString("Politics", 50, 260);
+	mainFont.drawString("Paris", 50, 320);
+	mainFont.drawString("DreamWorks", 50, 380);
+	mainFont.drawString("Uber", 50, 440);
+	mainFont.drawString("Trump", 50, 500);
+	mainFont.drawString("Obama", 50, 560);
+	mainFont.drawString("Dogs", 50, 620);
+	mainFont.drawString("Christmas", 50, 680);
+	mainFont.drawString("Total Count: - number of tweets -", 300, 200);
+	tweetFont.drawString("- list of all tweets -", 300, 300);
 }
